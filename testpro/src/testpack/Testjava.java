6677372a906b1e66d7f4c7b887890b888cc4dfc1
Soniya <soniyaagarwal.tech@gmail.com>
2024-04-25 22:26:24 -0400

    this is my first commit

---
+++ testpro/src/testpack/Testjava.java
@@ -0,0 +1,10 @@
+package testpack;
+
+public class Testjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is my Test Project1");
+	}
+
+}
